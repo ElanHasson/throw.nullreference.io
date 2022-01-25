62275e7d49431bcca86ce62a7a8b1a8123b958c4
Elan Hasson <234704+ElanHasson@users.noreply.github.com>
2022-01-25 20:32:36 +0000

    Add platform social share

--- src/js/index.js
+++ src/js/index.js
@@ -8,3 +8,23 @@ library.add(faGlobe, faClock); // Add icons that you need.
 document.addEventListener('DOMContentLoaded', () => {
   dom.watch();
 });
+
+window.shareIntent = function (title, text, url) {
+  const shareData = {
+    title: title,
+    text: text,
+    url: url
+  }
+
+  const btn = document.querySelector('.platform-sharer');
+  
+
+  // Share must be triggered by "user activation"
+  btn.addEventListener('click', async () => {
+    try {
+      await navigator.share(shareData)
+    } catch(err) {
+      console.log('Error: ' + err)
+    }
+  });
+}
